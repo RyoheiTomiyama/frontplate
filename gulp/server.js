f66c3c19f8df932c49516832729a8b7025039917
RyoheiTomiyama <RyoheiTomiyama@users.noreply.github.com>
2015-04-15 14:49:36 +0900

    Update server.js
    ブラウザで各階層を確認できるようにする。

--- gulp/server.js
+++ gulp/server.js
@@ -12,7 +12,6 @@ module.exports = function () {
                 directory: false,
                 middleware: [
                     rewrite([
-                        '^[^\\.]*$ /index.html [L]'
                     ])
                 ]
             },
